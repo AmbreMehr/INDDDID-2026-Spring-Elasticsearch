0fb7a466ee9a0a4490be986a6379e415d9806f13
AmbreMehr <ambre.mehr@gmail.com>
2026-06-11 16:36:26 +0200

    Initialisation du projet Spring

---
+++ src/test/java/fr/diiage/org/indddid/_6/elasticsearch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diiage.org.indddid._6.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
